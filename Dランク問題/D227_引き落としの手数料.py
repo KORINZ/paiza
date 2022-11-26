3395ffd9aa736139b0503ec48a8bd099bf6f99c0
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-27 01:02:13 +0900

    Finish question D227

---
+++ Dランク問題/D227_引き落としの手数料.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def balance(n_x_y: List[int]) -> List[str]:
+        n, x, y = n_x_y
+        people = []
+
+        for i in range(1, n + 1):
+            if i % x == 0 and i % y != 0:
+                people.append("A")
+            elif i % y == 0 and i % x != 0:
+                people.append("B")
+            elif i % y == 0 and i % x == 0:
+                people.append("AB")
+            else:
+                people.append("N")
+
+        return people
+
+
+if __name__ == '__main__':
+    input_line = list(map(int, input().split()))
+    for i in Solution().balance(input_line):
+        print(i)
