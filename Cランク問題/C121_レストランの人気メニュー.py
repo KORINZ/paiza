d477f75714087a0943cab3c417341db75f71b44e
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-27 02:22:34 +0900

    Finish question C121

--- Cランク問題/C121_レストランの人気メニュー.py
+++ Cランク問題/C121_レストランの人気メニュー.py
@@ -4,7 +4,24 @@ from typing import List
 class Solution:
     @staticmethod
     def top_ten(n_c: List[int], r_cal: List[List[int]]) -> str or int:
-        print(r_cal)
+        n, c = n_c
+
+        top_list = [i + 1 for i in range(10)]
+        current_cal = 0
+        top_ten_had = []
+
+        for i in r_cal:
+            current_cal += i[1]
+
+            if i[0] in top_list and current_cal <= c:
+                top_ten_had.append(i[0])
+            if current_cal >= c:
+                break
+
+        if sorted(top_ten_had) == top_list:
+            return 'Yes'
+        else:
+            return len(top_ten_had)
 
 
 if __name__ == '__main__':
