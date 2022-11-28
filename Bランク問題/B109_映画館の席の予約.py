6fdb56b89a5acdec1449431c49215e3011d16186
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-28 20:15:49 +0900

    Revise typo

--- Bランク問題/B109_映画館の席の予約.py
+++ Bランク問題/B109_映画館の席の予約.py
@@ -6,7 +6,7 @@ from typing import List, Set, Tuple
 
 class Solution:
     @staticmethod
-    def cinmena(n_h_w_p_q: List[int], reserved: Set[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    def cinema(n_h_w_p_q: List[int], reserved: Set[Tuple[int, int]]) -> List[Tuple[int, int]]:
         n, h, w, p, q = n_h_w_p_q
 
         available = {(x, y) for x in range(h) for y in range(w) if (x, y) not in reserved}
@@ -19,5 +19,5 @@ class Solution:
 if __name__ == '__main__':
     N_H_W_P_Q = list(map(int, input().split()))
     P_Q = {tuple(map(int, input().split())) for i in range(N_H_W_P_Q[0])}
-    for i in Solution().cinmena(N_H_W_P_Q, P_Q):
+    for i in Solution().cinema(N_H_W_P_Q, P_Q):
         print(*i)
