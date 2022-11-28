277ba7128f6e4d8e58c9f107fb7babbc53ce4e4c
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-28 17:56:17 +0900

    Update code structure

--- Bランク問題/B109_映画館の席の予約.py
+++ Bランク問題/B109_映画館の席の予約.py
@@ -1,16 +1,15 @@
 """
 https://paiza.jp/career/challenges/533/retry
 """
-from typing import List
+from typing import List, Set, Tuple
 
 
 class Solution:
     @staticmethod
-    def cinmena(n_h_w_p_q: List[int], p_q: List) -> List:
+    def cinmena(n_h_w_p_q: List[int], reserved: Set[Tuple[int, int]]) -> List[Tuple[int, int]]:
         n, h, w, p, q = n_h_w_p_q
-        reserved = {i for i in p_q}
-        available = {(x, y) for x in range(h) for y in range(w) if (x, y) not in reserved}
 
+        available = {(x, y) for x in range(h) for y in range(w) if (x, y) not in reserved}
         min_manhattan_distance = min([abs(p - x) + abs(q - y) for (x, y) in available])
         best_seats = [(x, y) for (x, y) in available if abs(p - x) + abs(q - y) == min_manhattan_distance]
 
@@ -19,6 +18,6 @@ class Solution:
 
 if __name__ == '__main__':
     N_H_W_P_Q = list(map(int, input().split()))
-    P_Q = [tuple(map(int, input().split())) for i in range(N_H_W_P_Q[0])]
+    P_Q = {tuple(map(int, input().split())) for i in range(N_H_W_P_Q[0])}
     for i in Solution().cinmena(N_H_W_P_Q, P_Q):
         print(*i)
