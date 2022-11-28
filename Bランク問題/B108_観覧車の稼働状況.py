a1a2267bac75e772dd7e7a17b32f6b833303d6c6
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-28 20:22:45 +0900

    Update method names

--- Bランク問題/B108_観覧車の稼働状況.py
+++ Bランク問題/B108_観覧車の稼働状況.py
@@ -7,12 +7,12 @@ from collections import deque
 
 class Solution:
     @staticmethod
-    def rotate(limit: Deque[int], gondora_id: Deque[int]) -> None:
+    def rotate_gondora(limit: Deque[int], gondora_id: Deque[int]) -> None:
         limit.append(limit.popleft())
         gondora_id.append(gondora_id.popleft())
 
     @staticmethod
-    def gondora(n_m: List[int], limit: List[int], group: List[int]) -> List[int]:
+    def check_gondora(n_m: List[int], limit: List[int], group: List[int]) -> List[int]:
 
         n, m = n_m
         limit, group = deque(limit), deque(group)
@@ -25,13 +25,13 @@ class Solution:
             if limit[0] >= group[0]:  # lower or equal to the limit of 1 gondora
 
                 gondora_id_dict[gondora_id[0]] += group[0]
-                Solution.rotate(limit, gondora_id)
+                Solution.rotate_gondora(limit, gondora_id)
                 group.popleft()
 
             else:  # too many people for 1 gondora
                 gondora_id_dict[gondora_id[0]] += limit[0]
                 group[0] = group[0] - limit[0]
-                Solution.rotate(limit, gondora_id)
+                Solution.rotate_gondora(limit, gondora_id)
 
         return list(gondora_id_dict.values())
 
@@ -40,5 +40,5 @@ if __name__ == '__main__':
     N_M = list(map(int, input().split()))
     A = [int(input()) for i in range(N_M[0])]
     B = [int(input()) for i in range(N_M[1])]
-    for i in Solution().gondora(N_M, A, B):
+    for i in Solution().check_gondora(N_M, A, B):
         print(i)
